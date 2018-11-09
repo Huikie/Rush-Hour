620266ecd906ca84554d1635f47b606b468d24f3
hhiiddee1 <hidde.van.oijen@gmail.com>
2018-11-09 14:20:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from board import Board
 def main():
     board = self.board
-    counter1 = 1
+    counter1 = 0
     with open("Game_1.txt", "r") as file:
         for row in file:
-            counter2 = 1
+            counter2 = 0
             counter1 += 1
             for char in row:
                 counter2 += 1
