6bff7ddb8a16fbdc47115be672f7caaa32809270
hhiiddee1 <hidde.van.oijen@gmail.com>
2018-11-14 16:27:48 +0100

    Update Classes/board.py

--- Classes/board.py
+++ Classes/board.py
@@ -12,12 +12,14 @@ class Board:
 ['#', '#', '#', '#', '#', '#', '#', '#']
 ]
         self.cars = Cars()
-    # def move(self, car, moves):
-    #     if self.cars.cars[car][0][0]== self.cars.cars[car][1][0]:
-    #         # horzontaal
-    #         for i in range(0, moves + 1):
-    #             for cordinate in self.cars.cars[car]:
-    #
-    #         return True
-    #     else:
-    #         # verticaal
+    def move(self, car, moves):
+        if self.cars.cars[car][0][0]== self.cars.cars[car][1][0]:
+            # horzontaal
+            for i in range(0, moves + 1):
+                for cordinate in self.cars.cars[car]:
+                    if self.board[cordinate[0]][cordinate[0 + i]] == ".":
+
+
+            return True
+        else:
+            # verticaal
