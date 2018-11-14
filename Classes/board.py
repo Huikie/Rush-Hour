f04a53c444f7bf486a08de0462e0f4d86746f856
hhiiddee1 <hidde.van.oijen@gmail.com>
2018-11-14 17:27:21 +0100

    update

--- Classes/board.py
+++ Classes/board.py
@@ -15,6 +15,7 @@ class Board:
     def move(self, car, moves):
         if self.cars.cars[car][0][0]== self.cars.cars[car][1][0]:
             # horzontaal
+            print("horizontal")
             for i in range(0, moves + 1):
                 for cordinate in self.cars.cars[car]:
                     if not self.board[cordinate[0]][cordinate[1] + i] == ".":
@@ -25,11 +26,29 @@ class Board:
 
             return True
         else:
-            for i in range(0, moves + 1):
-                for cordinate in self.cars.cars[car]:
-                    if not self.board[cordinate[0] + i][cordinate[1]] == ".":
-                        print ("invalid move")
-                        return False
-            return True
+            if moves > 0:
+                for i in range(1, moves + 1):
+                    print ("verical")
+                    print(moves)
+                    for cordinate in self.cars.cars[car]:
+                        print(cordinate)
+                        print(self.board[cordinate[0] + i][cordinate[1]])
+                        print (car)
+                        if self.board[cordinate[0] + i][cordinate[1]] != "." and self.board[cordinate[0] + i][cordinate[1]] != car :
+                            print ("invalid move")
+                            return False
+                return True
+            elif moves < 0:
+                for i in range(-1, moves -1, -1):
+                    print ("verical")
+                    print(moves)
+                    for cordinate in self.cars.cars[car]:
+                        print(cordinate)
+                        print(self.board[cordinate[0] + i][cordinate[1]])
+                        print (car)
+                        if self.board[cordinate[0] + i][cordinate[1]] != "." and self.board[cordinate[0] + i][cordinate[1]] != car :
+                            print ("invalid move")
+                            return False
+                return True
             #self.board[cordinate]
             # verticaal
