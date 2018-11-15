6d24a49e4beab492c7101401f622fc4f83e33446
hhiiddee1 <hidde.van.oijen@gmail.com>
2018-11-15 14:50:29 +0100

    update load board moved to class board

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 from Classes.board import Board
 def main():
     board = Board()
-    counter1 = 0
-    dict = {}
     with open("Game_1.txt", "r") as file:
-        for row in file:
-            counter2 = 0
-            counter1 += 1
-            for char in row:
-                counter2 += 1
-                if char.isalpha():
-                    if char in dict:
-                        dict[char].append([counter1, counter2])
-                    else:
-                        dict[char] = [[counter1, counter2]]
-                if char != "\n":
-                    board.board[counter1][counter2] = char
-    board.cars.add(dict)
+        board.load_board(file)
+
     print(board.board)
     print(board.cars.cars)
-    # if board.move("C",1) == True:
-    #     print("yes")
-    # print(board. board)
-    # print(board.cars.cars)
-    if board.move("C",1)== True:
-        print("valid move")
+    if board.move("C",1) == True:
+        print("yes")
     print(board. board)
     print(board.cars.cars)
 
