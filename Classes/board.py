02f07d4f256aa38afa1ec3b0ef3f8785b642a833
hhiiddee1 <hidde.van.oijen@gmail.com>
2018-11-15 14:05:16 +0100

    update all cars can move now

--- Classes/board.py
+++ Classes/board.py
@@ -18,32 +18,38 @@ class Board:
         if self.cars.cars[car][0][0]== self.cars.cars[car][1][0]:
             # moves cars to the right
             if moves > 0:
-                print("horizontaal_positief")
                 # checks if move is valid
                 for i in range(1, moves + 1):
                     for cordinate in self.cars.cars[car]:
                         if self.board[cordinate[0]][cordinate[1] + i] != "." and self.board[cordinate[0]][cordinate[1] + i] != car :
                             print ("invalid move")
                             return False
+                for cordinate in self.cars.cars[car]:
+                    self.board[cordinate[0]][cordinate[1]] = "."
+                for cordinate in self.cars.cars[car]:
+                    self.board[cordinate[0]][cordinate[1]+ moves] = car
+                for cordinate in self.cars.cars[car]:
+                    lijst.append([cordinate[0], cordinate[1] + moves])
+                    self.cars.cars[car] = lijst
                 return True
             elif moves < 0:
                 # moves car to the left
-                print("horizontaal_negatief")
                 # checks if move is valid
                 for i in range(-1, moves -1, -1):
                     for cordinate in self.cars.cars[car]:
-                        #print(cordinate)
-                        print(self.board[cordinate[0]][cordinate[1 + i]])
-                        print(self.board[cordinate[0] + i][cordinate[1]])
-                        #print (car)
                         if self.board[cordinate[0]][cordinate[1] + i] != "." and self.board[cordinate[0]][cordinate[1] + i] != car :
                             print ("invalid move")
                             return False
+                for cordinate in self.cars.cars[car]:
+                    self.board[cordinate[0]][cordinate[1]] = "."
+                for cordinate in self.cars.cars[car]:
+                    self.board[cordinate[0]][cordinate[1]+ moves] = car
+                for cordinate in self.cars.cars[car]:
+                    lijst.append([cordinate[0], cordinate[1] + moves])
+                    self.cars.cars[car] = lijst
                 return True
         # Move cars that are positioned vertically
         else:
-            print("\n")
-            print(self.cars.cars[car])
             # moves cars down
             if moves > 0:
                 # checks if move is valid
@@ -52,7 +58,6 @@ class Board:
                         if self.board[cordinate[0] + i][cordinate[1]] != "." and self.board[cordinate[0] + i][cordinate[1]] != car :
                             print ("invalid move")
                             return False
-            #lijst.append([cordinate[0], cordinate[1] + 1])
                 for cordinate in self.cars.cars[car]:
                     self.board[cordinate[0]][cordinate[1]] = "."
                 for cordinate in self.cars.cars[car]:
@@ -67,13 +72,15 @@ class Board:
             elif moves < 0:
                 # checks if move is valid
                 for i in range(-1, moves -1, -1):
-                    print ("verical")
-                    print(moves)
                     for cordinate in self.cars.cars[car]:
-                        print(cordinate)
-                        print(self.board[cordinate[0] + i][cordinate[1]])
-                        print (car)
                         if self.board[cordinate[0] + i][cordinate[1]] != "." and self.board[cordinate[0] + i][cordinate[1]] != car :
                             print ("invalid move")
                             return False
+                for cordinate in self.cars.cars[car]:
+                    self.board[cordinate[0]][cordinate[1]] = "."
+                for cordinate in self.cars.cars[car]:
+                    self.board[cordinate[0]+ moves][cordinate[1]] = car
+                for cordinate in self.cars.cars[car]:
+                    lijst.append([cordinate[0] + moves, cordinate[1]])
+                    self.cars.cars[car] = lijst
                 return True
