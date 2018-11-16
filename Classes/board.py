e815c9230d0d7581adada2a66b64a5c93b3a5c54
10810765 <10810765@users.noreply.github.com>
2018-11-16 20:56:50 +0100

    board.py redundant code removed and duplicates removed

--- Classes/board.py
+++ Classes/board.py
@@ -30,75 +30,63 @@ class Board:
                     if char != "\n":
                         self.board[counter1][counter2] = char
             self.cars.add(dict)
+
     def move(self, car, moves):
         lijst = []
         # Move cars that are positioned horizontally
-        if self.cars.cars[car][0][0]== self.cars.cars[car][1][0]:
+        if self.cars.cars[car][0][0] == self.cars.cars[car][1][0] and moves > 0:
             # moves cars to the right
-            if moves > 0:
                 # checks if move is valid
-                for i in range(1, moves + 1):
-                    for cordinate in self.cars.cars[car]:
-                        if self.board[cordinate[0]][cordinate[1] + i] != "." and self.board[cordinate[0]][cordinate[1] + i] != car :
-                            print ("invalid move")
-                            return False
-                for cordinate in self.cars.cars[car]:
-                    self.board[cordinate[0]][cordinate[1]] = "."
-                for cordinate in self.cars.cars[car]:
-                    self.board[cordinate[0]][cordinate[1]+ moves] = car
-                for cordinate in self.cars.cars[car]:
-                    lijst.append([cordinate[0], cordinate[1] + moves])
-                    self.cars.cars[car] = lijst
-                return True
+            for i in range(1, moves + 1):
+                return validMoveHorizontal(self, car, moves, i, lijst)
                 # moves car to the left
-            elif moves < 0:
+                # Moves < 0
+            else:
                 # checks if move is valid
                 for i in range(-1, moves -1, -1):
-                    for cordinate in self.cars.cars[car]:
-                        if self.board[cordinate[0]][cordinate[1] + i] != "." and self.board[cordinate[0]][cordinate[1] + i] != car :
-                            print ("invalid move")
-                            return False
-                for cordinate in self.cars.cars[car]:
-                    self.board[cordinate[0]][cordinate[1]] = "."
-                for cordinate in self.cars.cars[car]:
-                    self.board[cordinate[0]][cordinate[1]+ moves] = car
-                for cordinate in self.cars.cars[car]:
-                    lijst.append([cordinate[0], cordinate[1] + moves])
-                    self.cars.cars[car] = lijst
-                return True
+                    return validMoveHorizontal(self, car, moves, i, lijst)
         # Move cars that are positioned vertically
         else:
             # moves cars down
             if moves > 0:
                 # checks if move is valid
                 for i in range(1, moves + 1):
-                    for cordinate in self.cars.cars[car]:
-                        if self.board[cordinate[0] + i][cordinate[1]] != "." and self.board[cordinate[0] + i][cordinate[1]] != car :
-                            print ("invalid move")
-                            return False
-                for cordinate in self.cars.cars[car]:
-                    self.board[cordinate[0]][cordinate[1]] = "."
-                for cordinate in self.cars.cars[car]:
-                    self.board[cordinate[0]+ moves][cordinate[1]] = car
-                for cordinate in self.cars.cars[car]:
-                    lijst.append([cordinate[0] + moves, cordinate[1]])
-                    self.cars.cars[car] = lijst
-                return True
+                    return validMoveVertical(self, car, moves, i, lijst)
+
                 #self.cars.cars[car] = lijst
                 #print(self.cars.cars[car])
             # moves cars up
-            elif moves < 0:
+            # moves < 0
+            else:
                 # checks if move is valid
                 for i in range(-1, moves -1, -1):
-                    for cordinate in self.cars.cars[car]:
-                        if self.board[cordinate[0] + i][cordinate[1]] != "." and self.board[cordinate[0] + i][cordinate[1]] != car :
-                            print ("invalid move")
-                            return False
-                for cordinate in self.cars.cars[car]:
-                    self.board[cordinate[0]][cordinate[1]] = "."
-                for cordinate in self.cars.cars[car]:
-                    self.board[cordinate[0]+ moves][cordinate[1]] = car
-                for cordinate in self.cars.cars[car]:
-                    lijst.append([cordinate[0] + moves, cordinate[1]])
-                    self.cars.cars[car] = lijst
-                return True
+                    return validMoveVertical(self, car, moves, i, lijst)
+
+#Function that checks if a horizontal move is valid
+def validMoveHorizontal(self, car, moves, i, lijst):
+    for cordinate in self.cars.cars[car]:
+        if self.board[cordinate[0]][cordinate[1] + i] != "." and self.board[cordinate[0]][cordinate[1] + i] != car :
+            print ("invalid move")
+            return False
+        for cordinate in self.cars.cars[car]:
+            self.board[cordinate[0]][cordinate[1]] = "."
+        for cordinate in self.cars.cars[car]:
+            self.board[cordinate[0]][cordinate[1]+ moves] = car
+            lijst.append([cordinate[0], cordinate[1] + moves])
+            self.cars.cars[car] = lijst
+        return True
+
+
+#Function that checks if a vertical move is valid
+def validMoveVertical(self, car, moves, i, lijst):
+    for cordinate in self.cars.cars[car]:
+        if self.board[cordinate[0] + i][cordinate[1]] != "." and self.board[cordinate[0] + i][cordinate[1]] != car :
+            print ("invalid move")
+            return False
+        for cordinate in self.cars.cars[car]:
+            self.board[cordinate[0]][cordinate[1]] = "."
+        for cordinate in self.cars.cars[car]:
+            self.board[cordinate[0]+ moves][cordinate[1]] = car
+            lijst.append([cordinate[0] + moves, cordinate[1]])
+            self.cars.cars[car] = lijst
+        return True
