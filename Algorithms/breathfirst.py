2bd35d456ff1cd732f6a99e1473f4f913c2a9175
hhiiddee1 <hidde.van.oijen@gmail.com>
2018-11-29 12:51:10 +0100

    update

--- Algorithms/breathfirst.py
+++ Algorithms/breathfirst.py
@@ -9,64 +9,41 @@ class Breathfirst:
         self.counter = 0
         self.archive =[]
 
-    def __str__():
-        
+
     def move(self):
+        counter = 0
         for j in self.boards:
             #print(j.board)
             for move in range(-1, -5, -1):
-                self.board_temp = copy.deepcopy(self.boards[self.counter])
+                self.board_temp = copy.deepcopy(self.boards[counter])
                 for car in self.board.cars.cars.keys():
                     if self.board_temp.move(car, move) != False:
                         if self.board_temp.board not in self.archive:
+                            self.board_temp.parent = counter
                             self.boards.append(copy.deepcopy(self.board_temp))
-                            self.boards[len(self.boards)-1].parent = self.counter
                             self.archive.append(copy.deepcopy(self.board_temp.board))
-                            #print(self.board_temp.board)
+                            print(len(self.boards))
+                            print(self.board_temp.parent)
+                            print(self.board_temp.board)
                             if self.board_temp.won() == True:
                                 return True
                     self.board_temp.move(car, -move)
-                        #print(self.boards[self.counter].board)
+
             for move in range(1, 5):
-                self.board_temp = copy.deepcopy(self.boards[self.counter])
+                self.board_temp = copy.deepcopy(self.boards[counter])
                 for car in self.board.cars.cars.keys():
                     if self.board_temp.move(car, move) != False:
                         if self.board_temp.board not in self.archive:
+                            self.board_temp.parent = counter
                             self.boards.append(copy.deepcopy(self.board_temp))
-                            self.boards[len(self.boards)-1].parent = self.counter
                             self.archive.append(copy.deepcopy(self.board_temp.board))
-                            #print(self.board_temp.board)
+                            print(len(self.boards))
+                            print(self.board_temp.parent)
+                            print(self.board_temp.board)
                             if self.board_temp.won() == True:
                                 return True
                     self.board_temp.move(car, -move)
-                        #print(self.boards[self.counter].board)
-            #for i in self.boards:
-                #print(i.board)
 
-            self.counter += 1
-            #print(self.boards[self.counter].board)
-            #print(self.counter)
-            #if self.board_temp.won() == True:
-            #    print(len(self.boards))
-            #    return True
-            # if 1000 % self.counter == 0:
-            #     print(self.counter)
-            if self.counter == 1000000:
+            counter = counter + 1
+            if counter == 1000000:
                 return True
-
-
-    # def breathfirstSolver():
-    #     moveCount = 0
-    #     while not gameWon():
-            #If zz can be moved to >, move the car to > and gameWon()
-                #return gameWon()
-            #else:
-                #Do a random move and check again
-
-
-    # def gameWon():
-    #     if ....
-            # If car zz reaches >, end breathfirst and game won
-            #return True
-        # else
-            #return false
