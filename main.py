32da6ed2c75b96d94f40919394fe344d61b6c2b0
10810765 <10810765@users.noreply.github.com>
2018-12-17 12:43:55 +0100

    Change to main.py

--- main.py
+++ main.py
@@ -5,32 +5,36 @@ import sys
 import os.path
 
 def main():
+
     algorithmBF = Breadthfirst()
     algorithmRB = RandomBound()
     board = Board()
     i = 0
 
-    # Checks if file board file exists.
+    # Checks if file board file exists
     if os.path.exists("Data/"+sys.argv[1]):
         with open("Data/"+sys.argv[1], "r") as file:
 
-            # Gives the correct board size.
+            # Gives the correct board size
             for i, l in enumerate(file):
                 i += 1
             file.seek(0)
 
-            # Starts Random bound algorithm.
+            # Starts Random bound algorithm
             if sys.argv[2] == "randombound":
                 algorithmRB.board.load_board(file, i)
-                algorithmRB.randomBound(argv[3], i)
+                if sys.argv[3] > 0:
+                    algorithmRB.randomBound(sys.argv[3], i)
+                else:
+                    print("Invalid iteration size.")
 
-            # Starts breadthfirst algorithm.
+            # Starts breadthfirst algorithm
             elif sys.argv[2] == "breadthfirst":
                 algorithmBF.board.load_board(file, i)
                 algorithmBF.boards.append(algorithmBF.board)
                 algorithmBF.breadthFirst(i)
             else:
-                print("invalid algorithm")
+                print("Invalid algorithm.")
     else:
         print("No such file '{}'".format(sys.argv[1]))
 
